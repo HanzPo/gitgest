f144dc278733315498ab6f2fb84da249f8ed9f7b
Hanz Po <hanznathanpo@gmail.com>
2023-10-14 14:22:26 -0400

    Format JSX

--- frontend/src/pages/Landing.jsx
+++ frontend/src/pages/Landing.jsx
@@ -16,46 +16,46 @@ const Landing = () => {
 
   return (
     <div>
-    <div className="header">
-      <h1 id="companyName">company name here </h1>
-    </div>
-    <div className="center">
-      <div id="leftSide">
-        <img
-          src="src/img/robot.png"
-          alt="Image of Robot"
-          width="600"
-          height="500"
-        />
+      <div className="header">
+        <h1 id="companyName">company name here </h1>
       </div>
-      <div className="rightSide">
-        <h1 id="colourHead">Summarize Commits</h1>
-        <h1 id="whiteHead">With AI</h1>
-        <p id="paragraph">
-        We use Co:here’s API to summarize all Github commits since your last
-        one.
-        </p>
-        <div className="inputContainer">
-          <input
-            className="textInput"
-            id="textInput1"
-            value={repoLink}
-            onChange={handleRepoLinkChange}
-            placeholder="Enter your GitHub repository link here"
+      <div className="center">
+        <div id="leftSide">
+          <img
+            src="src/img/robot.png"
+            alt="Image of Robot"
+            width="600"
+            height="500"
           />
-          <input
-            className="textInput"
-            id="textInput2"
-            value={username}
-            onChange={handleUsernameChange}
-            placeholder="Enter your username here"
-          />
-          <Link to={`/summary?url=${repoLink}&poi=${username}`}>
-            <button id="goButton"></button>
-          </Link>
         </div>
-      </div>  
-    </div>
+        <div className="rightSide">
+          <h1 id="colourHead">Summarize Commits</h1>
+          <h1 id="whiteHead">With AI</h1>
+          <p id="paragraph">
+            We use Co:here’s API to summarize all Github commits since your last
+            one.
+          </p>
+          <div className="inputContainer">
+            <input
+              className="textInput"
+              id="textInput1"
+              value={repoLink}
+              onChange={handleRepoLinkChange}
+              placeholder="Enter your GitHub repository link here"
+            />
+            <input
+              className="textInput"
+              id="textInput2"
+              value={username}
+              onChange={handleUsernameChange}
+              placeholder="Enter your username here"
+            />
+            <Link to={`/summary?url=${repoLink}&poi=${username}`}>
+              <button id="goButton"></button>
+            </Link>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
