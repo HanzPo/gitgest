1592048de4e6f471e46274ca7aba4dea6914e2e2
Simra Khan <simrakhan7605@gmail.com>
2023-10-14 12:12:04 -0400

    added robot image

--- frontend/src/pages/Landing.jsx
+++ frontend/src/pages/Landing.jsx
@@ -3,16 +3,22 @@ import "./Landing.css";
 const Landing = () => {
   return (
     <div className="center">
-      <h1 id="colourHead">Summarize Commits</h1>
-      <h1 id="whiteHead">With AI</h1>
-      <p id="paragraph">We use Co:here’s API to summarize all Github commits since your last one.</p>
-      <div className="inputContainer">
-        <input className="textInput" id="textInput1" placeholder="Enter your GitHub repository link here"/>
-        <input className="textInput" id="textInput2" placeholder="Enter your username here"/>
-        <a href="/summary">
-          <button id="goButton"></button>
-        </a>
+      <div id="leftSide">
+      <img src="src/img/robot.png" alt="Image of Robot" />
       </div>
+      <div className="rightSide">
+        <h1 id="colourHead">Summarize Commits</h1>
+        <h1 id="whiteHead">With AI</h1>
+        <p id="paragraph">We use Co:here’s API to summarize all Github commits since your last one.</p>
+        <div className="inputContainer">
+          <input className="textInput" id="textInput1" placeholder="Enter your GitHub repository link here"/>
+          <input className="textInput" id="textInput2" placeholder="Enter your username here"/>
+          <a href="/summary">
+            <button id="goButton"></button>
+          </a>
+        </div>
+      </div>
+      
     </div>
   );
 };
