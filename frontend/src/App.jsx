76deb25202404df21d39af1ace82ea0e3f50df9c
Yi Jia Huang <yijh1705@gmail.com>
2023-10-14 09:24:25 -0400

    css for title and tect inputs for the landing page and half finished css for button

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Landing from "./pages/Landing";
 import Summary from "./pages/Summary";
 import "./App.css";
+import './fonts/SpaceGrotesk/SpaceGrotesk-Bold.ttf';
+import './fonts/SpaceGrotesk/SpaceGrotesk-Regular.ttf';
 
 function App() {
   return (
--- frontend/src/pages/Landing.jsx
+++ frontend/src/pages/Landing.jsx
@@ -2,14 +2,17 @@ import "./Landing.css";
 
 const Landing = () => {
   return (
-    <div>
-      <h1>Landing page</h1>
-      <p>Testing landing page</p>
-      <input />
-      <input />
-      <a href="/summary">
-        <button>Click</button>
-      </a>
+    <div className="center">
+      <h1 id="colourHead">Summarize Commits</h1>
+      <h1 id="whiteHead">With AI</h1>
+      <p id="paragraph">We use Co:here’s API to summarize all Github commits since your last one.</p>
+      <div className="inputContainer">
+        <input className="textInput" id="textInput1" placeholder="Enter your GitHub repository link here"/>
+        <input className="textInput" id="textInput2" placeholder="Enter your username here"/>
+        <a href="/summary">
+          <button id="goButton"></button>
+        </a>
+      </div>
     </div>
   );
 };
