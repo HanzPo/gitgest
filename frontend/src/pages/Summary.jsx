ed1f8657b270c9c921e10b0379f8abb2b8edd39d
Hanz Po <hanznathanpo@gmail.com>
2023-10-14 15:04:27 -0400

    Complete last commit count

--- frontend/src/pages/Summary.jsx
+++ frontend/src/pages/Summary.jsx
@@ -5,25 +5,27 @@ import axios from "axios";
 
 const Summary = () => {
   const [searchParams, setSearchParams] = useSearchParams();
+  const [sinceLastCommit, setSinceLastCommit] = useState(0);
   const [summary, setSummary] = useState("");
   const url = searchParams.get("url");
   const poi = searchParams.get("poi");
 
   useEffect(() => {
     axios
-      .get(`http://127.0.0.1:5000/?url=${url}&poi=${poi}`)
+      .get(`http://127.0.0.1:5000/receive_repo?url=${url}&poi=${poi}`)
       .then((response) => {
-        console.log(response);
+        setSinceLastCommit(response.data.since_last_commit);
+        console.log(response.data);
       })
       .catch((error) => {
-        
+        console.log(error);
       });
   }, []);
 
   return (
     <div>
       <h1>Summarization of Commits Since</h1>
-      <p>{url}</p>
+      <p>{sinceLastCommit}</p>
     </div>
   );
 };
